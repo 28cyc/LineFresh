1cae370e62a00ea804ccf2b7487a79ed960fcbc1
28cyc <by890228@gmail.com>
2021-11-07 21:45:26 +0800

    upload

---
+++ LineFresh/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace LineFresh
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+			services.AddSingleton<LineBotConfig, LineBotConfig>((s) => new LineBotConfig
+			{
+				channelSecret = Configuration["LineBot:channelSecret"],
+				accessToken = Configuration["LineBot:accessToken"]
+			});
+
+			services.AddHttpContextAccessor();
+			//services.AddRazorPages();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseMvc();
+
+			//app.UseEndpoints(endpoints =>
+			//{
+			//	endpoints.MapControllerRoute(
+			//		name: "api",
+			//		pattern: "api/{controller=Home}/{action=Index}/{id?}");
+			//	endpoints.MapRazorPages();
+			//});
+		}
+	}
+}
