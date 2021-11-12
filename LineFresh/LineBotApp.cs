831b04912c0588de99e13277c90a920f604ec355
28cyc <82566955+28cyc@users.noreply.github.com>
2021-11-12 17:26:40 +0800

    Update LineBotApp.cs

--- LineFresh/LineBotApp.cs
+++ LineFresh/LineBotApp.cs
@@ -386,10 +386,10 @@ namespace LineFresh
 					Layout = BoxLayout.Vertical,
 					Contents = new IFlexComponent[]
 					{
-						new IconComponent
-						{
-							Url = "https://cdn-icons-png.flaticon.com/512/1946/1946488.png"
-						},
+						//new IconComponent
+						//{
+						//	Url = "https://cdn-icons-png.flaticon.com/512/1946/1946488.png"
+						//},
 						new TextComponent
 						{
 							Text = $"雲林縣虎尾鎮中山路61號",
